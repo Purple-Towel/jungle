89ae3c54bd66cf90b0fe1abe57481a0552a5d4c8
Angad Grewal <angadgrewal31@gmail.com>
2020-05-29 03:16:04 +0000

    added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :orders
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
